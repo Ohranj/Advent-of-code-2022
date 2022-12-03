3b0b784be1558700f246e3dea6cbd35113a4b18e
Alex Dorrington <ajdorrington18@gmail.com>
2022-12-03 21:36:03 +0000

    Day 2

---
+++ day2.js
@@ -0,0 +1,100 @@
+const data = "A Z C X A Z A Z C Y C Y A Z A Y C Y A Y A Z A Z A Z A Y A Z A Y C Y C X A Y C Y C Y C X A Z C Y C X A X A Y A Z A Y A Y C X C X B Y C X C X A Y A Z A Z A X A Z A Z C Y A Z A Z A Y C X C Y C X B X C Z A Y A Z A Z A Z A Y A Y C X A Y A Z C Y A Y A Y A Z A Z C Y A Z C Y A Y A Z A Z C Y B Z A Z A Z A Z A Z C X C X A Y A X A Y A Z A Z C X A Z C X A Z C Y C X A Z A Z A Z A Z C X C Y A Z A Z C Y A Z B Z C X A Z A X C X A Z C Z A Z C X A Z A Y C X C Z A Z C X C Y A Z B Z B Y A X A X A X A Z A Z A X A Z A X A Z A Z C X C X B Z A Z A Y A Z A Z A Z A Z C X C X C X A Z A Z A Z A Z A X A Y A Y C X C X B Z C X A X A Z A Z C X C Z A Z A Z C Y A X A Z C Y A X A Y A Y A Y C Y A Z A Z C X C X C X C X B Z C Y C X C X A Y A Y A Z A Z A X C X A Z B Z A Z C X A Y A Z A Z A Y A Y A Z C X A Y C X C X A Z A Y C X A Z A Z A Y A Z A Z C Y C Z A Y A Z C X C X A Z A Z C Y A Y A Z A Y A Z C X A Z A Z C X A Y A Y C Y A Z C Y A Z A Z C Y A Y B Z C Y C X C X A Z C Y A X C Y A Z A Y A Z C X C X C Y C Y A Y A Z C X C X A Y A X A Y B Z A Y C Y C X C X A X C X B Z A X C Y C X C X A X A Z B Z A Z A Z A Y A X A Z C X A X C Y A Z A Z A X A Z A Z C X C X A Z A Z A Z A X A Z A X B Y A Z A Y C Y A Z C X A Z C X A Y A Z C X C Y A Y C Y A Z C X A Z A Z C X A Y A Z A X A Z A Z A Z B Y C X A X A Z A Z C Y C Y C X C X C X A X A X A Z A X A Y A Z A Y A Z C Y C Z A Y A Z A Z A X A Z A Z C X C Y A Y C X C X A Z C X C X C X A X A Z A Z A Z A Z B Y A Y A Y A Z C X A Y A Z C X A Z C Z A Y C X A Z B Z A Y A X A Z C X A Z A Z A Z A Z A Z B Z A Z C X A Y C X A Z A Z A Y A Z B Z C X A Y C Y A Z A Z C X C X A Y C X C Y B Z A Y C X A Y C X A X A Y A Z A Z A Y C X A X C X B Y A Z A Y B Y A Y C X A Z A Z C X C Y A Y C X C Y A Y A Z A X B Z C X A Z A Y A Z B Z A Z A X C Y A X A Z A Y C Y A Z C Y A Z C X C X A Y C X C X A Y A Z A Z A Y A X C Y A Z A Z C X A X A Z C Z A Z C Y A Z C X A Z A Z A X C X C X C Y B Z B Y C Y A Y A X A Z C X A Y A Y A Z A Z C Z C X C X C X A Z C X A Z A Z A Y C Y C X C X C X A Y C X B Z C X A Z C Y A Y C Y A Z A Z C X A X A Z A Y A Z B X C Z A Z C Z A X C X C X C X C X A Z A Y A Z A Z A Z A Z A Z C Z C Y C Y C X C Y A Z C X C X A Z A Z C X C Y C Y A Z A X C Y C Y C X A Y C X A Z A Z A Y C Y A Y C X C X A Z A Z C Y A Z C X A Y A Y A Z C Y A Z C X A Y A Z A Z A Z A Z C Y A Y C Y A Z A Z A X A Y A Y A Z C Y A Z A X A Z B Z C X C X C Y A Z A Z C X C Z A Z C X C Y A Z B Z A Z B Z A X A Y A Z A Z A Z A Z C Y A Z A Z A Z B Z A Z C X C X A Z C X A X A Z A Y A Y A X A Z A Z A Z A Z B Z A Z C Y C Y C X C X B Z C Y A Z C Y A Z A Y C Y A X A Y C X A Y C X A Z A Z B Z A Y C Y C X A Z A Z C X A Y A Z A Z A X A Y A Y A Y A Z A Y A Z C X A X A Z A Z C Y A Z C X A Z C Y A Y A Z A Z A Z A Z C Y A X A Z A Z A Y A Z B Z A X A Y C X C X A Y A Z C Y A Z A Y A Y A Z A Y A Y A Z A Z A Z C X A Z A Y A Z A Z C Y A Z C X A Y C X C Y A Z C Y A Z A Y C Y C Y A Z C X C X C X A Z A Z A Y C X A Z A X A Z A Z C X A Z C Y A Y A Z A Y A Z A Z C Y A Z C X A Z A X A Z A Z C X A Z A Z C Y C X A Z C Y C X C X A Z A Z A Z A X C X A Z A Z C Y A Y C X A Y C X C Y A Y A Z C X A Z B Y A X B Z A Y A X C Y A Z A X A Z A Z C Y A Z B Z C X C X B Y A Z A Z A Z C Y C X A Z A Y A Y A Y C Y C X A Z A X A X A X A Z A Z A Z A Z C X C X A Z C X A Y C X B Z A Z A Z C X A Z C X C X C X A Y C Y C X A Y C X C Y B Z A Z C X A Z A X B Z A Y B Y A Z A Z A X A Z A X A Z A Z C X C Y A X C Y C X A Z A Z A Z C Y A X A Y A Z A Y C X B Z A Z A Y C Z C X A Z A Z A Z B Z A X C X A Z A Z B Z A Z A Z B Z A Z C X A Z C X A Z C X A Z C Y A Z A X A Y A Y C X A Y C X B Z A Z A Z A Z A Z C X A Z C X A Z A Y A Z B Z A Y C Z A Y C X A Z A Z A Y B Z A X C Y A Z A Y A Z A Y A Y A Z A Z A Z A X A Z C Y A Y A X A Y C X A Y C X A X C X A Z C Y A Y A Z A Z A X A Y C X C X A Y A X A Z B Z A Y A Z A X A Z B Y A Y A Y A Z C X A Z A Z A Y A Z C Z A Z A Z A Z A Z C Y C Y A Y B Z C Y A Y C Y A Z A Z A Z A Z C Y A Z A Z C Y C X A Y A Y A Z C X C Z C X C X A Z A Z A Y A Z A X C Y A Z A Z C Y C X A X A Z A Z A X C X C X C X A Z A X C X C Y A X A Z C X A Z A Z C X A Y A Z A Z A Y A Z C X A Z A Z C X A Y A Z A Z C X A Z A Y A Z C Y A X A Z A Z C X A Z A Y C Y B Z A Z A Y C X A Z B Z A Z C Z A X A Z A Z C Y A Z A Y C X C Y A Z A Z A Y A X C Y A Y C X C Y A Z C Y A Z C X A Z A Z A X A Z A Z B X A X A Z C Y A Z A X C X A Z A Z C X C Y C Y A X A Y C Y A Y A Z A Z A Z A Z A Y C X C X C Y C X A Z A X B Z B Y C X C Y A Y A Z A Y C X C Z A Z A Y C Y C X A Z A Z A Y C X C Z C Y A Z C X C Y A X A X A Y A Z B Z A X A Y A Y C X C Y A Z A X A Z A X A Y A Z A Z A Z C X A Z A Z A Z C Z C Y C Y A Z C Y C Y C Y C X A Z C X C X A Z A Y A Z A Z A X A Y A Y C X C X A Z A Z A Z A Z A Y A Z A Z A Z A Z A X A X A Y A X C Y A Y A Z C X A Y A Y A Z A Z A Z C X A Z C X C X C Y A Y A Z A Y A Z C X C X A Z A Z A Z C X A X A Z A Z C Y C Y A Y A Y A Z A Z C Y C X C Y A X C Y C X C Y A Z A Z A X C Y C Y A Z A Y C X A X B Z A Z C X A Y A X A Z A Z A Z C Y C X A Z C X A Z A Z A Y A Z A Y A X A Z C Z A Z A Z A Z A X A Z A X C X A Z A Z C Y A X A Z C X C X A Y A Z A Z C X B X A Z C Y A Z C X A Y A Z C X A X A Z A X A Z C X A Z A X A Z C X C Y A Y A X A X A Z C Y C Y A Y A Y A Y A X A Z A Z A Z C Y C Y A Y B X B X A Z C X C X A Z C Y C X A Z A Z A Z A Z C X A Z A Z C Y A Y C Y A Z C Y C Y A Z C X A Z A X A Z C X C X A Y B Z A Y C Y A Z C Y A X C X A Y C X A Z C Z C Y A Z C X C Y A Z A X A Z A Z A Z C X A Z A Z A Z A Z A Z A Z A Z A Z A Z A Y A Z A X A Y C Y B Y C X B Z A Z A Z A Y B Z A Z A Z C X C Y C X A Y A X C X C Y A Y C X C Y A Z A Z A Z A Y C X A Y C X B Z A Z A Y A Z A Z A Y C X A Z C X C Y A Y A Z B Z C X A Y C X A Y A Z C X A X C X B Y C X A Z A Y A Z A Y A X C X C X A Y C X A Y A Y A X B Y A Y C X C X A Y B Z B X B Z A Y A Z C Y A Y B Y A Z C X A Z A Z A Z A Z B Z C X C Y A Z C Y C Y A X C X A Z A Z A Z C X C X C X A Y C Z C Z A Z C X A Y A Z A Z A Z C X A Z A X A Z A Z A Z A Z A Y C Y C X A X A Y C X A X A Z A Z C X A Z A X A Z A Z A X A Z A Z A X A Z A X B Y A Y A Y C Y A Z A Y C X A Z A Y A X C Y B X C Y A Z C X A Y A Z A Y A X C Y A Z A Z C Y C X A Z C X A Y C X A Z A Y A Z A Z A Z A Y A Z C X C X A X C X C X A Z C X A Z C Y C X A Z A Z A Z C X A X C Y A Z C Y A X A Z C X A Z A Z A X A Z C X B X A Z A Z A Z C X A Y A Y A X C Y C Y A Z A Y A Z A Z C X A X A Y A Z A Z A Z A Z B Z C X C X C X A Z C Z A X C X A Z C Y A Z A Z A Y A Y C X A Z A X A Z A Z A Z A Z C Y A Z A Y A X A X A Z C X A X A X A Z A Y C X A Z A Z A Y A Z B Z C X C X C Z C Y C X A Z C Y A Z C Z A Z A Y A Y A X A X A Z A Y A Y A Y A Y A Z C Y A Z A Z C X A Z A Z C Y A Y C X A Y C X A Z B Z A X B X A Y A X A Y B Z A Y A Z C Y C Y A Z A X A Z A Z C Z A Z A Y C X A Y C X A X A Y C Y A Y A Z A Z C X C X B Z A Z A Z A X C X C Y A Z A Z A X C X C Z A Z C Y A Y B Z C Y A Z C X A X A Z A Z A Z A Y C Y A Z C Y A Z A Z A Z A Z A Y C X A Y C Y B Z A Z C X C Y A Z C X A Z C X C Z A Z C X C X A Z A Z A Y A Y A Y C X A Y A Z A Z A Z A Z A Z A Z A Y A Z A X C Y A Z A Z A Z A Y A Z A Z A Z A Z C X B Z A Z A Y A Y A X A Z A Z C Z A Z C X A Y A X B Z A Z A Z A Z C Y C Z C X A Z C Y C Y C Y C X B Z A Z A Z C Z A X A Z A Z A Z C X A Z A Z C Y C Y A Z A Z C X A Y C Y C Y A Z A Z A X A Z A Z A X A X C X A Z A X C X C X A Z A X A Z C X C X C X C Y A Z A Z A X A Z A Y A Z A Z C Y A Z A Z A Z A X A Z C X A X A Z A Z A Z A Z A Z A Z A Z A Y A X A Y A X C Y A Z A Z C X A Z A Z A Z A X A Z A Z A Y C X A Y A Z C X A X A Y A Z A Z A X A Z A Y C X A Y A Z A Z A Z A Z C X C Y A Z B Z C X A Z A Z C Y C Y C X A X C Y B Y A Z A Z A Z C Y A Z A X A Y A Z A Z A Z C Y A Z C Y C X A Z A Z A Z A Z A Z A X A Z A Z C X A Z A Y C Z A Z A Z A Z B X C X A Z A Z A Z A Z C X A Z A Z A Y A X C X C Y A X A Y C X A Z A Z C X C X A Z C X A Z A Y C X A Z B X B Y A X C Y A X A Y C Y A Z A Z A Z C X A Z A X A Z C Y A Z A Z C X A Z A Z A Y A Z A X A Y A Z C X C Y B X C Y A Y A Z C X A Z C X C Y A X A X A Z A Z C Y A Y A Y A Z A Z C X A X C X A X A Y C X A Z A Z B Y A Z A Z A Z A Z A Z A X A Y A X A Z A Z C X A Y A Z C Y C X C X C X C X C Y A X A X C Y A X A Y A Y B Z A Z B Y C Y A X A Y A Z A Z A Z A X C X A Z A Z A Z A Z A Z A Y A Z B Z A Z A Z A Y C Y C Y C X A Z A X C X A Y B Z C X A Z C X C Y C Y A X A X C X A Z A Z A Y A X A X A Z C Y B X A Z A Z A Z A Z A Y A Y A Z C Y C Y A X A Z A Z C Y A Y A Y A X A Y A X A X A X A Z A Z A Y A Z C X A Z A Y A Z A Z A Y A Z A Z C X A Z A Y A Z A Z A Z A Z A Z A Z A Y C X B Z C Z A Z A Y A X A X C Y C Y A X C X B Z A Z C X A Z A Z A Z A Z A Z A Z A Z A Y A Z A Z A Y A Z A Y A Y A Z A Z A Y A Z C Y A Z A Z A Z C X C X A Z C Y A Z A Z C X A Z C X A Y A Z C Y A Z A Z C X C Y A Z C X C X A X A Y C Y A Y A Z C X A Z A Z A X A Z C Y A Z C X A Z C X A Z C X A Y C Y A X A Z A Z C Y A X A Z A Y B Z A Z A X A Z A Z A X C X A Z A Z C Y A Z A Y A Z C Y A Z A Y A Z C Z A Y A Z A Y C Y A Z C X A X B Z C X C X A Z A Z A Y A X A Z A Z B Z A Z C Z A X A Z A Z A Z A Z A X A X A Z A X A Z A Z A Z A X C X C Y A X C X C X A Z A X C Y B Z A Z A Z C X B X A Z A Z C X C X B Z C Y A Z A Z C X A X A Y C Y C Y B Z A Y C X A Z A Z A Y C X A Z A Z C X C Y A Z C X C Y C Y C X C Y A Z C Y A Z C X A Z C Y C Z A Z B Z A Z C X C X B Y B Z C Y C X C X A Y C X C X A Z A Y A Z C X A X A Z A Z A Z A Z A Z A Z A Z C X C X A Z C Y C X A Z B Z A Z A Z A X C X A Y A Z A Z A Y A X C Y B X A Z A X C Y C X C X C Y C Y A Z A Z C Y A Z A Y C Y A Y C X A Z C X C Y C Y A Z A Z A X A Y A Z A X A Z A Z A Z C Y A Z A Z C X C Y A Z A Z A Z C Y C X A Z C X A Z A Z A Z A X A Z A Z A Y B Z A Z A Z A Z C Y A Z B Z A Z C Z A Z A Y C X C Y C X C X A Z A Z A Y A Y A Z A Y B Z C Y A Y A Z C X A Z A Z C Y A Y A Z A Y C Y A Z A Z A X B Z A Z A X C X A Z C Y C Y A Z B Y A Y A Z A Z A Z A Z C X C X A Z C X A Y A Z A Z C Y A Z A Z C Y C X B Z A Y A Y C X C X A Z A X B Z A Z C Y A Z A Y A Z A Z A Y C Y C X A Z C X A Z C Y A Z C Y A Y A Z A Y A Z C X A Z A Z C X A Z B Y A Z A Z C Y C X C X A Z A Z C X B Z A Y A Z A Y A Z A Z A Y A X C X C X A X A Z A Y A Y A Z A Y A Z A Z C Y A X A Z A Z C X A Z A X B X C X A Z A Y B Z C X C Y A Z B Z C Y A Z A Z A X A Z A Z A Z A Z A Z A Z A Z";
+
+const dataArray = data.split(' ');
+const gamesArray = [];
+
+while (dataArray.length) {
+    gamesArray.push(dataArray.splice(0, 2));
+}
+
+
+
+/**
+ * Question 1
+ */
+const game1 = {
+    moveScores: {
+        'X': 1,
+        'Y': 2,
+        'Z': 3
+    },
+    rules: {
+        'A': {
+            'X': 'draw',
+            'Y': 'won',
+            'Z': 'lost'
+        },
+        'B': {
+            'X': 'lost',
+            'Y': 'draw',
+            'Z': 'won'
+        },
+        'C': {
+            'X': 'won',
+            'Y': 'lost',
+            'Z': 'draw'
+        }
+    },
+    lost: 0,
+    draw: 3,
+    won: 6,
+    score: 0
+}
+
+for (const item of gamesArray) {
+    const [oppoMove, myMove] = [...item];
+    const result = game1.rules[oppoMove][myMove]
+    game1.score += game1[result]
+    game1.score += game1.moveScores[myMove]
+}
+console.log(game1.score)
+
+
+
+/**
+ * Question 2
+ */
+
+const game2 = {
+    moveScores: {
+        'X': 1,
+        'Y': 2,
+        'Z': 3
+    },
+    rules: {
+        'X': 'lose',
+        'Y': 'draw',
+        'Z': 'won'
+    },
+    outcomes: {
+        'A': {
+            'draw': 'X',
+            'won': 'Y',
+            'lose': 'Z'
+        },
+        'B': {
+            'lose': 'X',
+            'draw': 'Y',
+            'won': 'Z'
+        },
+        'C': {
+            'won': 'X',
+            'lose': 'Y',
+            'draw': 'Z'
+        }
+    },
+    lose: 0,
+    draw: 3,
+    won: 6,
+    score: 0
+}
+
+for (const item of gamesArray) {
+    const [oppoMove, myMove] = [...item];
+    const resultText = game2.rules[myMove];
+    game2.score += game2[resultText]
+    const moveScore = game2.outcomes[oppoMove][resultText]
+    game2.score += game2.moveScores[moveScore];
+
+}
+console.log(game2.score)
